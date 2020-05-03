7a3d890298ea6f088ff563fd8f8edcc329967beb
Estanipp <estanipp@gmail.com>
2020-05-03 18:10:56 -0300

    Updates

---
+++ precache-manifest.5e47479d31103f8a191998bd94b453ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48f43e33a6092af2a85f11527f42cd1",
+    "url": "/PaginaReact/index.html"
+  },
+  {
+    "revision": "a4382b9a538c65ede8fa",
+    "url": "/PaginaReact/static/js/2.bf013692.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PaginaReact/static/js/2.bf013692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c1ea4014f0d3565df6",
+    "url": "/PaginaReact/static/js/main.fc43edbd.chunk.js"
+  },
+  {
+    "revision": "63c27f28541fb7bc2268",
+    "url": "/PaginaReact/static/js/runtime-main.8baaaf65.js"
+  },
+  {
+    "revision": "e32e575801982488cee75b78004ae6eb",
+    "url": "/PaginaReact/static/media/4045.e32e5758.jpg"
+  }
+]);
